489e0e1ff2a26d84413f8496b15e59fef9649ad2
baoping.dan <baoping.dan@pjbest.com>
2019-11-18 20:01:12 +0800

    rm unused imports

--- src/main/java/com/demo/position/util/PositionUtil.java
+++ src/main/java/com/demo/position/util/PositionUtil.java
@@ -4,11 +4,8 @@ import com.demo.position.entity.Position;
 import com.demo.position.exception.ValidationException;
 
 import java.math.BigDecimal;
-import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
-import java.util.concurrent.atomic.AtomicBoolean;
-import java.util.concurrent.atomic.AtomicReference;
 
 public class PositionUtil {
 
